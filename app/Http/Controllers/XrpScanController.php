a868a8fce82b58d1f781d8e65d37a956dbb43514
hollynight127626 <hollynight127626@gmail.com>
2024-08-01 06:02:07 +0300

    add User-Agent

--- app/Http/Controllers/XrpScanController.php
+++ app/Http/Controllers/XrpScanController.php
@@ -15,7 +15,7 @@ class XrpScanController extends Controller
         try {
             $response = $client->get($uri, [
                 'headers' => [
-                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' //add this
                 ]
             ]);
 
